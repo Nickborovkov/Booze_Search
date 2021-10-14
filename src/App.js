b6db0e394e1948f2d0c22775d7026295a9c2c61c
Nickborovkov <borovckov@yandex.ru>
2021-10-14 17:29:31 +0300

    app init, added lazyloading

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {lazy, Suspense} from "react";
+import AppHeader from "./components/header/appHeader";
+import {Route, Switch} from "react-router-dom";
+import Preloader from "./components/common/preloader/Preloader";
+import HomePage from "./components/homePage/HomePage";
 
-function App() {
+const Cocktails = lazy( () =>
+    import(/* webpackChunkName: "Cocktails" */"./components/cocktailsList/Cocktails") )
+const SpecificCocktail = lazy( () =>
+    import(/* webpackChunkName: "SpecificCocktail" */"./components/cocktailsList/specificCocktail/SpecificCocktail") )
+const Ingredients = lazy( () =>
+    import(/* webpackChunkName: "Ingredients" */"./components/ingredientsList/Ingredients") )
+const ErrorPage = lazy( () =>
+    import(/* webpackChunkName: "Error" */"./components/common/errorPage/ErrorPage") )
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div>
+        <AppHeader/>
+          <Suspense fallback={<Preloader/>}>
+              <Switch>
+                  <Route exact path='/cocktails'
+                         render={() => <Cocktails/>}/>
+                  <Route exact path='/cocktails/cocktail/:id'
+                         render={(props) => <SpecificCocktail {...props}/>}/>
+                  <Route path='/ingredients'
+                         render={() => <Ingredients/>}/>
+                  <Route exact path='/'
+                         render={() => <HomePage/>}/>
+                  <Route exact path='*'
+                         render={() => <ErrorPage/>}/>
+              </Switch>
+          </Suspense>
+      </div>
+  )
 }
 
 export default App;
