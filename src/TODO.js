a9c8de9561208bc1620bd52831e11c26d65c3146
Nickborovkov <borovckov@yandex.ru>
2021-10-15 16:17:42 +0300

    added TODO.js

---
+++ src/TODO.js
@@ -0,0 +1,6 @@
+// `todo : input validation with formik + +yup`
+// `todo : combine ingredients and cocktails pages`
+// `todo : search for ingredients`
+// `todo : link from ingredient to cocktails with this ingredient`
+// `todo : ----//----- from specific Cocktail`
+// `todo : back to top button`
