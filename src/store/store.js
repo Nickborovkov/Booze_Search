f46489ef20fee7910f16ba9e57c65224c2ca530b
Nickborovkov <borovckov@yandex.ru>
2021-10-15 16:21:35 +0300

    refactored

--- src/store/store.js
+++ src/store/store.js
@@ -2,12 +2,10 @@ import {applyMiddleware, combineReducers, createStore} from "redux";
 import thunk from "redux-thunk";
 import commonReducer from "./commonReducer";
 import cocktailsReducer from "./cocktailsReducer";
-import ingredientsReducer from "./ingredientsReducer";
 
 const reducers = combineReducers({
     common: commonReducer,
     cocktails: cocktailsReducer,
-    ingredients: ingredientsReducer,
 })
 
 const store = createStore(reducers, applyMiddleware(thunk))
