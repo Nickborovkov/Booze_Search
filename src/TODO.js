815f04c367955d895f63c0fcecd3ffaecdfa8d42
Nickborovkov <borovckov@yandex.ru>
2021-10-15 17:14:25 +0300

    update TODO.js

--- src/TODO.js
+++ src/TODO.js
@@ -3,4 +3,8 @@
 // `todo : search for ingredients`
 // `todo : link from ingredient to cocktails with this ingredient`
 // `todo : ----//----- from specific Cocktail`
-// `todo : back to top button`
+// `todo : redux toolkit`
+// `todo : fast typing input bug fix (maybe validate with enter or button)`
+// `todo : create README.md`
+// `todo : change icons`
+// `todo : default value in inputs?`
