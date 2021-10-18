0ec626bd11b3502632e60ec8c53c5e335004f898
Nickborovkov <borovckov@yandex.ru>
2021-10-18 17:42:26 +0300

    added homePage

---
+++ src/components/homePage/HomePage.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+
+const HomePage = (props) => {
+    return (
+        <Box>
+            <Typography
+                variant="h3"
+                align='center'
+                mt={3}
+                sx={{color: `#00e676`}}
+            >
+                Find yourself a nice booze receipt
+            </Typography>
+            <Box
+                sx={{display: `flex`, justifyContent: `center`}}
+                mt={10} mb={10}
+            >
+                <Button
+                    variant='contained'
+                    color={'button'}
+                    onClick={ () => {props.history.push(`/cocktails/ByName`)} }
+                >Get started</Button>
+            </Box>
+        </Box>
+
+    )
+}
+
+export default HomePage
