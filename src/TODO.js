b1ba7bc3ee880704588989f40fa1a75b7db97a70
Nickborovkov <borovckov@yandex.ru>
2021-10-18 17:36:24 +0300

    update TODO.js

--- src/TODO.js
+++ src/TODO.js
@@ -1,11 +1,5 @@
-// `todo : input validation with formik + +yup`
-// `todo : combine ingredients and cocktails pages`
-// `todo : search for ingredients`
-// `todo : link from ingredient to cocktails with this ingredient`
-// `todo : ----//----- from specific Cocktail`
-// `todo : redux toolkit`
-// `todo : fast typing input bug fix (maybe validate with enter or button)`
+// `todo : error image`
+// `todo : layout bug`
+
 // `todo : create README.md`
-// `todo : change icons`
-// `todo : default value in inputs?`
-// `todo : query filters`
+// `todo : redux toolkit`
