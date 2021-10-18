85858884be4ca479ecdc9902e81a1b9326c67836
Nickborovkov <borovckov@yandex.ru>
2021-10-18 17:46:03 +0300

    searchField moverd to separate component, addedvalidation with formik + yup

---
+++ src/components/common/searchField/SearchField.jsx
@@ -0,0 +1,80 @@
+import React from "react";
+import {Formik} from "formik";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import * as yup from 'yup'
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+
+const SearchField = ({searchType, items, setItems}) => {
+
+    const regexp = /^[a-zA-Z0-9_., -]*$/
+
+    const validationSchema = yup.object().shape({
+        items: yup
+            .string()
+            .required(`Field can't be empty`)
+            .max(150, `Max length is1 50 symbols`)
+            .matches(regexp, `Only english letters and digits`)
+    })
+
+    return (
+        <Formik
+            initialValues={{
+                items: items
+            }}
+            validateOnChange
+            onSubmit={((values) => {
+                setItems(values.items)
+            })}
+            validationSchema={validationSchema}
+        >
+            {({
+                  values, errors, handleChange,
+                  handleBlur, handleSubmit, isValid, dirty
+              }) => (
+                <form>
+                    <Grid container
+                          justifyContent="center"
+                          sx={{width: `80%`, margin: `50px auto`}}
+                    >
+                        <Grid item xs={12} md={10}>
+                            <TextField
+                                error={errors.items ? true : false}
+                                label={errors.items || `Search for cocktails`}
+                                variant="outlined"
+                                name='items'
+                                value={values.items}
+                                onChange={handleChange}
+                                fullWidth
+                                size='small'
+                                color={`input`}
+                                onBlur={handleBlur}
+                                helperText={searchType === `byIngredient` ? `Requires full ingredient name` : ``}
+                            />
+                        </Grid>
+                        <Grid item xs={12} md={2}>
+                            <Box>
+                                <Button
+                                    disabled={!isValid && !dirty}
+                                    variant='contained'
+                                    color={'button'}
+                                    onClick={handleSubmit}
+                                    type='submit'
+                                    fullWidth
+                                    size='large'
+                                    sx={{display: `block`}}
+                                >
+                                    Search
+                                </Button>
+                            </Box>
+                        </Grid>
+                    </Grid>
+                </form>
+            )}
+
+        </Formik>
+    )
+}
+
+export default SearchField
