5996c4e8ec598cc8e1c823f7a949842c91f4a586
Nickborovkov <borovckov@yandex.ru>
2021-10-18 17:45:17 +0300

    added ingredients component

---
+++ src/components/ingredients/Ingredients.jsx
@@ -0,0 +1,68 @@
+import React, {useEffect} from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {getIngredientByName} from "../../store/boozeReducer";
+import Preloader from "../common/preloader/Preloader";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import CardActions from "@mui/material/CardActions";
+import Button from "@mui/material/Button";
+
+const Ingredients = (props) => {
+
+    const dispatch = useDispatch()
+    const ingredientName = props.match.params.name
+    const ingredient = useSelector(state => state.booze.ingredient)
+    const isLoading = useSelector(state => state.common.isLoading)
+
+    useEffect(()=>{
+        dispatch(getIngredientByName(ingredientName))
+    },[dispatch, ingredientName])
+
+    return (
+        <div>
+
+            {isLoading && <Preloader/>}
+
+            {!isLoading && ingredient &&
+            <Box sx={{display: `flex`, justifyContent: `center`}} pt={5} pb={5}>
+                <Card sx={{ maxWidth: `80%` }}>
+                    <CardContent>
+                        <Typography gutterBottom variant="h3" component="div">
+                            {ingredient.strIngredient}
+                        </Typography>
+                        <Typography gutterBottom variant="h5" component="div">
+                            <span style={{color: `#00e676`}}>Is alcoholic: </span>{ingredient.strAlcohol || `No info`}
+                        </Typography>
+                        <Typography gutterBottom variant="h5" component="div">
+                            <span style={{color: `#00e676`}}>ABV°: </span>{ingredient.strABV || `No info`}
+                        </Typography>
+                        <Typography gutterBottom variant="p" component="div">
+                            <span style={{color: `#00e676`}}>Description: </span>{ingredient.strDescription  || `No info`}
+                        </Typography>
+                    </CardContent>
+                    <CardActions>
+                        <Button
+                            color='button'
+                            variant="contained"
+                            size="small"
+                            onClick={ () => {props.history.goBack()} }
+                        >
+                            Go back
+                        </Button>
+                        <Button
+                            color='button'
+                            variant="contained"
+                            size="small"
+                        >
+                            More booze with this ingredient
+                        </Button>
+                    </CardActions>
+                </Card>
+            </Box>}
+        </div>
+    )
+}
+
+export default Ingredients
